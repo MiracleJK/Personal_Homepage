25328bda6de44d11b14b763f0a8f2a3bc09bfb15
Unknown <playerjoko@163.com>
2020-11-03 19:47:57 +0800

    显示图片
    设置媒体路径和网址

--- gallery/models.py
+++ gallery/models.py
@@ -4,5 +4,11 @@ from django.db import models
 
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=50)
+    description = models.CharField(default='在这里写简介', max_length=50)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='作品标题', max_length=50)
+
+    def __str__(self):
+        return self.title
+
 
